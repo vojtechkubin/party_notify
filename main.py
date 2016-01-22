c7abcdfd82e433311197781517cf0e1f22d80357
Vojtech Kubin <vojtech.kubin@seznam.cz>
2016-01-22 21:04:24 +0100

    encoding_fix_2
    Signed-off-by: Vojtech Kubin <vojtech.kubin@seznam.cz>

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from person import Person, Person_encoder
 
 
 def main():
-	data = fake_db.load('data.json')
+	data = (fake_db.load('data.json'))
 	print(data)
 	data2 = {}
 	persona = Person('first_name', 'second_name', str(date.today()), 'kokos', 'mail@hacking.fg', '777999555', 'facebook')
 	print(Person_encoder().encode(persona))
-	data2['persona'] = str(persona)
+	data2['persona'] = (Person_encoder().encode(persona))
 	fake_db.save(data2, 'data2.json')
 
 if __name__ == '__main__':
