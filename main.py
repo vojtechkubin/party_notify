09f1d16b71f170428ce1621b6dbdc59790224877
Vojtech Kubin <vojtech.kubin@seznam.cz>
2016-01-22 17:52:08 +0100

    add person
    Signed-off-by: Vojtech Kubin <vojtech.kubin@seznam.cz>

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import fake_db
+from datetime import date
+from person import Person, Person_encoder
 
 
-def main ():
+def main():
 	data = fake_db.load('data.json')
 	print(data)
-	fake_db.save(data, 'data2.json')
+	data2 = {}
+	persona = Person('first_name', 'second_name', str(date.today()), 'kokos', 'mail@hacking.fg', '777999555', 'facebook')
+	print(Person_encoder().encode(persona))
+	data2['persona'] = persona
+	fake_db.save(data2, 'data2.json')
 
 if __name__ == '__main__':
 	main()
-	
---
+++ person.py
@@ -0,0 +1,16 @@
+from json import JSONEncoder
+
+
+class Person(object):
+	def __init__(self, first_name, second_name, birthday_date, nameday_date, mail, tel_number, facebook):
+		self.first_name = first_name
+		self.second_name = second_name
+		self.birthday_date = birthday_date
+		self.nameday_date = nameday_date
+		self.mail = mail
+		self.tel_number = tel_number
+		self.facebook = facebook
+
+class Person_encoder(JSONEncoder):		
+	def default(self, o):
+		return o.__dict__
