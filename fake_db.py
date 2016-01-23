d0aee52af1acfc0897a0d6ff137b41eea31b6eb8
Matej Dujava <mdujava@gmail.com>
2016-01-23 18:45:46 +0100

    serialize array od persons
    Signed-off-by: Matej Dujava <mdujava@gmail.com>

--- fake_db.py
+++ fake_db.py
@@ -1,19 +1,31 @@
+from person import Person_encoder
 import json
 
 
 def save(data, file_name = 'database'):
-	file_name = file_extension(file_name)
-	with open(file_name, 'w') as data_file:
-		data_file.write(json.dumps(data, sort_keys = True, indent = 4))
-		
+
+    tmp = {}
+
+    print(data)
+
+    for item in data:
+        
+        tmp[item] = Person_encoder().encode(data[item])
+
+    print(tmp)
+
+    file_name = file_extension(file_name)
+    with open(file_name, 'w') as data_file:
+        data_file.write(json.dumps(tmp, sort_keys = True, indent = 4))
+
 def load(file_name = 'database'):
-	file_name = file_extension(file_name)
-	with open(file_name) as data_file:
-		data = json.load(data_file)
-	return data
+    file_name = file_extension(file_name)
+    with open(file_name) as data_file:
+        data = json.load(data_file)
+    return data
 
 def file_extension(file_name):
-	result = file_name.strip()
-	if '.json' not in result:
-		result += '.json'
-	return result
+    result = file_name.strip()
+    if '.json' not in result:
+        result += '.json'
+    return result
--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from person import Person, Person_encoder
 
 
 def main():
-	data = (fake_db.load('data.json'))
-	print(data)
-	data2 = {}
-	persona = Person('first_name', 'second_name', str(date.today()), 'kokos', 'mail@hacking.fg', '777999555', 'facebook')
-	print(Person_encoder().encode(persona))
-	data2['persona'] = (Person_encoder().encode(persona))
-	fake_db.save(data2, 'data2.json')
+    data = (fake_db.load('data.json'))
+    data2 = {}
+    persona = Person('first_name', 'second_name', str(date.today()), 'kokos', 'mail@hacking.fg', '777999555', 'facebook')
+    #data2['persona'] = str(Person_encoder(indent=4).encode(persona))
+    data2['persona'] = persona
+    data2['personb'] = persona
+    fake_db.save(data2, 'data2.json')
 
 if __name__ == '__main__':
-	main()
+    main()
